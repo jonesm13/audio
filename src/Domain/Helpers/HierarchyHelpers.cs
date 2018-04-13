e2b5a038b39839586d87a42aadd6250bc8ffc6a9
Matt Jones <matt.jones@referencepoint.co.uk>
2018-04-13 14:29:59 +0100

    Tidying

--- src/Domain/Helpers/HierarchyHelpers.cs
+++ src/Domain/Helpers/HierarchyHelpers.cs
@@ -2,9 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Diagnostics;
     using System.Linq;
-    using System.Linq.Expressions;
 
     public static class HierarchyHelpers
     {
