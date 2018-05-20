776df2c83d05d343a0bfdc234e3bab8af770bc98
Matt Jones <me@jonesm.io>
2018-05-20 17:36:50 +0100

    Removing old filestream access code

--- src/Domain/Adapters/SqlFileStreamAudioStore.cs
+++ src/Domain/Adapters/SqlFileStreamAudioStore.cs
@@ -1,10 +1,6 @@
 ﻿namespace Domain.Adapters
 {
     using System;
-    using System.Configuration;
-    using System.Data;
-    using System.Data.SqlClient;
-    using System.Data.SqlTypes;
     using System.IO;
     using System.Threading.Tasks;
     using Ports;
@@ -13,146 +9,18 @@
     {
         public Task StoreAsync(Guid id, string filename)
         {
-            const string insertSql = @"INSERT INTO AudioFile (Id, [Data])
-                    VALUES (@id, 0x00);
-
-                    SELECT [Data].PathName(),
-                        GET_FILESTREAM_TRANSACTION_CONTEXT()
-                    FROM AudioFile
-                    WHERE Id = @id;";
-
-            string connectionString = ConfigurationManager
-                .ConnectionStrings["audio-db"]
-                .ConnectionString;
-
-            using (SqlConnection conn = new SqlConnection(connectionString))
-            {
-                conn.Open();
-
-                using (SqlTransaction txn = conn.BeginTransaction())
-                {
-                    byte[] serverTxn;
-                    string serverPath;
-
-                    using (SqlCommand cmd = new SqlCommand(insertSql, conn))
-                    {
-                        cmd.Transaction = txn;
-                        cmd.Parameters.Add("@id", SqlDbType.UniqueIdentifier)
-                            .Value = id;
-
-                        using (SqlDataReader rdr = cmd.ExecuteReader())
-                        {
-                            rdr.Read();
-
-                            serverPath = rdr.GetSqlString(0)
-                                .Value;
-                            serverTxn = rdr.GetSqlBinary(1)
-                                .Value;
-                        }
-                    }
-
-                    const int blockSize = 1024 * 512;
-
-                    using (FileStream source = new FileStream(
-                        filename,
-                        FileMode.Open,
-                        FileAccess.Read))
-                    {
-                        using (SqlFileStream dest = new SqlFileStream(
-                            serverPath,
-                            serverTxn,
-                            FileAccess.Write))
-                        {
-                            byte[] buffer = new byte[blockSize];
-                            int bytesRead;
-                            while ((bytesRead = source.Read(
-                                       buffer,
-                                       0,
-                                       buffer.Length)) >
-                                   0)
-                            {
-                                dest.Write(buffer, 0, bytesRead);
-                                dest.Flush();
-                            }
-
-                            dest.Close();
-                        }
-
-                        source.Close();
-                    }
-
-                    txn.Commit();
-                }
-            }
-
             return Task.CompletedTask;
         }
 
-        public async Task DeleteAsync(Guid id)
+        public Task DeleteAsync(Guid id)
         {
-            const string deleteSql = @"DELETE FROM AudioFile WHERE Id = @id;";
-
-            string connectionString = ConfigurationManager
-                .ConnectionStrings["audio-db"]
-                .ConnectionString;
-
-            using (SqlConnection conn = new SqlConnection(connectionString))
-            {
-                conn.Open();
-
-                using (SqlTransaction txn = conn.BeginTransaction())
-                {
-                    using (SqlCommand cmd = new SqlCommand(deleteSql, conn))
-                    {
-                        cmd.Transaction = txn;
-                        cmd.Parameters.Add("@id", SqlDbType.UniqueIdentifier)
-                            .Value = id;
-
-                        await cmd.ExecuteNonQueryAsync();
-                    }
-
-                    txn.Commit();
-                }
-            }
+            return Task.CompletedTask;
         }
 
         public Task<Stream> Get(Guid id)
         {
-            const string getPathSql = @"SELECT [Data].PathName(),
-                GET_FILESTREAM_TRANSACTION_CONTEXT()
-                FROM AudioFile
-                WHERE Id = @id;";
-
-            string connectionString = ConfigurationManager
-                .ConnectionStrings["audio-db"]
-                .ConnectionString;
-
-            using (SqlConnection conn = new SqlConnection(connectionString))
-            {
-                conn.Open();
-
-                using (SqlTransaction txn = conn.BeginTransaction())
-                {
-                    byte[] serverTxn;
-                    string serverPath;
-
-                    using (SqlCommand cmd = new SqlCommand(getPathSql, conn))
-                    {
-                        cmd.Transaction = txn;
-                        cmd.Parameters.Add("@id", SqlDbType.UniqueIdentifier).Value = id;
-
-                        using (SqlDataReader rdr = cmd.ExecuteReader())
-                        {
-                            rdr.Read();
-
-                            serverPath = rdr.GetSqlString(0).Value;
-                            serverTxn = rdr.GetSqlBinary(1).Value;
-                        }
-                    }
-                }
-            }
-
-            return new Task<Stream>(() => new MemoryStream());
+            Stream result = new MemoryStream();
+            return Task.FromResult(result);
         }
     }
 }
